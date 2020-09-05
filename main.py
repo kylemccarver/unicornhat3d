af40b7034699456e9cf004dde24f1669041dd342
Kyle McCarver <kmccarver8@gmail.com>
2020-09-05 04:37:55 +0100

    Vertex shader, post processing, primitive assembly

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import time
 import glm
 import unicornhathd
 
+class Primitive(object):
+    def __init__(self, vertices):
+        self.vertices = vertices
+
 w, h = unicornhathd.get_shape()
 
 viewport = {
@@ -14,11 +18,7 @@ viewport = {
     "f": 100
 }
 
-verts = [
-    glm.vec4(-1, 1, 0, 1),
-    glm.vec4(-1, -1, 0, 1),
-    glm.vec4(1, -1, 0, 1)
-]
+
 
 M = glm.mat4(1)
 
@@ -28,19 +28,81 @@ V = glm.lookAt(glm.vec3(0, 0, 6),
 
 P = glm.perspective(glm.radians(45), viewport["w"]/viewport["h"], viewport["n"], viewport["f"])
 
-for v in verts:
-    mvc = P * V * M * v
-    ndc = mvc.xyz / mvc.w
+def main():
+    global M, V, P, viewport
 
-    screen = glm.vec3(0)
-    screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
-    screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
-    screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
-    
-    unicornhathd.set_pixel(screen.x, screen.y, 255, 0, 0)
+    verts = [
+        glm.vec4(-1, 1, 1, 1),
+        glm.vec4(-1, -1, 1, 1),
+        glm.vec4(1, -1, 1, 1),
 
-unicornhathd.brightness(0.2)
-unicornhathd.rotation(90)
-unicornhathd.show()
-time.sleep(2)
-unicornhathd.off()
+        glm.vec4(1, 1, 1, 1),
+        glm.vec4(-1, 1, 1, 1),
+        glm.vec4(1, -1, 1, 1)
+    ]
+
+    pipeline(verts)
+
+    try:
+        while True:
+            for v in verts:
+                mvc = P * V * M * v
+                ndc = mvc.xyz / mvc.w
+
+                screen = glm.vec3(0)
+                screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
+                screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
+                screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
+                
+                unicornhathd.set_pixel(screen.x, screen.y, 255, 0, 0)
+
+            unicornhathd.brightness(0.2)
+            unicornhathd.rotation(90)
+            unicornhathd.show()
+
+    except KeyboardInterrupt:
+        unicornhathd.off()
+
+
+def pipeline(vertices):
+    vertex_shader_out = vertex_shader(vertices)
+    vertex_post_processing_out = vertex_post_processing(vertex_shader_out)
+    primitives = primitive_assembly(vertex_post_processing_out)
+    for p in primitives:
+        print(p.vertices)
+
+
+def vertex_shader(vertices):
+    out = []
+    for v in vertices:
+        out.append(P * V * M * v)
+    return out
+
+
+def vertex_post_processing(vertices):
+    out = []
+    for v in vertices:
+        ndc = v.xyz / v.w
+
+        screen = glm.vec3(0)
+        screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
+        screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
+        screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
+        out.append(screen)
+    return out
+
+
+def primitive_assembly(vertices):
+    return [Primitive(vertices[i:i+3]) for i in range(0, len(vertices), 3)]
+
+
+def rasterize():
+    pass
+
+
+def fragment_shader():
+    pass
+
+
+if __name__ == "__main__":
+    main()
