575e274581ed9d9c58d54672667ec90eb03fead0
Kyle McCarver <kmccarver8@gmail.com>
2020-09-05 08:33:59 +0100

    Function reorganization, remove perspective division from vertex shader

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+import sys
 import time
 
 import glm
 import unicornhathd
 
+
 class Primitive(object):
     def __init__(self, vertices):
         self.vertices = vertices
+
+
+class Fragment(object):
+    def __init__(self):
+        self.screen = glm.vec3(0)
+        self.depth = sys.maxsize
+
 
 w, h = unicornhathd.get_shape()
 
@@ -17,8 +26,6 @@ viewport = {
     "n": 0.1,
     "f": 100
 }
-
-
 
 M = glm.mat4(1)
 
@@ -46,14 +53,10 @@ def main():
     try:
         while True:
             for v in verts:
-                mvc = P * V * M * v
-                ndc = mvc.xyz / mvc.w
+                mvp = P * V * M * v
 
-                screen = glm.vec3(0)
-                screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
-                screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
-                screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
-                
+                screen = clip_to_screen_space(mvp)
+               
                 unicornhathd.set_pixel(screen.x, screen.y, 255, 0, 0)
 
             unicornhathd.brightness(0.2)
@@ -68,27 +71,17 @@ def pipeline(vertices):
     vertex_shader_out = vertex_shader(vertices)
     vertex_post_processing_out = vertex_post_processing(vertex_shader_out)
     primitives = primitive_assembly(vertex_post_processing_out)
-    for p in primitives:
-        print(p.vertices)
+    fragments = rasterize(primitives)
 
 
 def vertex_shader(vertices):
-    out = []
-    for v in vertices:
-        out.append(P * V * M * v)
-    return out
+    return vertices
 
 
 def vertex_post_processing(vertices):
     out = []
     for v in vertices:
-        ndc = v.xyz / v.w
-
-        screen = glm.vec3(0)
-        screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
-        screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
-        screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
-        out.append(screen)
+        out.append(P * V * M * v)
     return out
 
 
@@ -96,12 +89,43 @@ def primitive_assembly(vertices):
     return [Primitive(vertices[i:i+3]) for i in range(0, len(vertices), 3)]
 
 
-def rasterize():
-    pass
+def rasterize(primitives):
+    for primitive in primitives:
+        screen_vertices = [clip_to_screen_space(v) for v in primitive.vertices]
+        min_x = min(screen_vertices, key=lambda v: v.x).x
+        min_y = min(screen_vertices, key=lambda v: v.y).y
+        max_x = max(screen_vertices, key=lambda v: v.x).x
+        max_y = max(screen_vertices, key=lambda v: v.y).y
+
+        for x in min_x:
+            for y in min_y:
+                pass
 
 
 def fragment_shader():
     pass
+
+
+def clip_to_screen_space(vertex):
+    ndc = vertex.xyz / vertex.w
+    screen = glm.vec3(0)
+    screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
+    screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
+    screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
+    return screen
+
+
+def barycentric(v1, v2, v3, p):
+    A_t = glm.length(glm.cross((v2 - v1), (v3 - v1))) * 0.5
+    A_1 = glm.length(glm.cross((v2 - p), (v3 - p))) * 0.5
+    A_2 = glm.length(glm.cross((p - v1), (v3 - v1))) * 0.5
+    A_3 = glm.length(glm.cross((v2 - v1), (p - v1))) * 0.5
+
+    a = A_1 / A_t
+    b = A_2 / A_t
+    g = A_3 / A_t
+
+    return a, b, g
 
 
 if __name__ == "__main__":
