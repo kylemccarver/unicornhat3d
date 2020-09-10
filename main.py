7f1d14ed3f3b078cd64b9ceec9d9827cd0171361
Kyle McCarver <kmccarver8@gmail.com>
2020-09-10 05:47:28 +0100

    Switch from barycentric coord calculation to determinate edge function, use top-left fill rule

--- main.py
+++ main.py
@@ -5,6 +5,16 @@ import glm
 import unicornhathd
 
 
+class Viewport(object):
+    def __init__(self, x, y, w, h, n, f):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+        self.n = n
+        self.f = f
+
+
 class Primitive(object):
     def __init__(self, vertices):
         self.vertices = vertices
@@ -16,32 +26,11 @@ class Fragment(object):
         self.depth = sys.maxsize
 
 
-eps = 0.00001
-
 unicornhathd.brightness(0.2)
 unicornhathd.rotation(90)
 
-w, h = unicornhathd.get_shape()
-
-viewport = {
-    "x": 0,
-    "y": 0,
-    "w": w,
-    "h": h,
-    "n": 0.1,
-    "f": 100
-}
-
-M = glm.mat4(1)
-
-V = glm.lookAt(glm.vec3(0, 0, 6), 
-               glm.vec3(0, 0, 0), 
-               glm.vec3(0, 1, 0))
-
-P = glm.perspective(glm.radians(45), viewport["w"]/viewport["h"], viewport["n"], viewport["f"])
-
 def main():
-    global M, V, P, viewport
+    global M, V, P, view
 
     verts = [
         glm.vec4(-1, 1, 1, 1),
@@ -53,17 +42,28 @@ def main():
         glm.vec4(1, -1, 1, 1)
     ]
 
+    w, h = unicornhathd.get_shape()
+
+    view = Viewport(0, 0, w, h, 0.1, 100)
+
+    M = glm.mat4(1)
+
+    V = glm.lookAt(glm.vec3(0, 0, 6), 
+                   glm.vec3(0, 0, 0), 
+                   glm.vec3(0, 1, 0))
+
+    P = glm.perspective(glm.radians(45), view.w / view.h, view.n, view.f)
 
     try:
         t = time.time()
-        while True:
-            unicornhathd.clear()
+        #while True:
+            #unicornhathd.clear()
             #dt = time.time() - t
             #t = time.time()
 
             #V = glm.rotate(V, 0.05, glm.vec3(0, 0, 1))
-            pipeline(verts)
-            unicornhathd.show()
+        pipeline(verts)
+            #unicornhathd.show()
 
     except KeyboardInterrupt:
         unicornhathd.off()
@@ -97,16 +97,27 @@ def rasterize(primitives):
         max_x = round(max(screen_vertices, key=lambda v: v.x).x)
         max_y = round(max(screen_vertices, key=lambda v: v.y).y)
 
+        v0 = screen_vertices[0]
+        v1 = screen_vertices[1]
+        v2 = screen_vertices[2]
+        area = edge_function(v0, v1, v2)
+
         for x in range(min_x, max_x):
             for y in range(min_y, max_y):
                 centroid = glm.vec3(x + 0.5, y + 0.5, 0)
-                v1 = glm.vec3(screen_vertices[0].xy, 0)
-                v2 = glm.vec3(screen_vertices[1].xy, 0)
-                v3 = glm.vec3(screen_vertices[2].xy, 0)
-                a, b, g = barycentric(v1, v2, v3, centroid)
-                interp = a + b + g
-                if interp >= 1 - eps and interp <= 1 + eps:
-                    unicornhathd.set_pixel(x, y, 255 * a, 255 * b, 255 * g)
+
+                w0 = edge_function(v1, v2, centroid)
+                w1 = edge_function(v2, v0, centroid)
+                w2 = edge_function(v0, v1, centroid)
+
+                bias0 = 0 if top_left_test(v1, v2) else -1
+                bias1 = 0 if top_left_test(v2, v0) else -1
+                bias2 = 0 if top_left_test(v0, v1) else -1
+
+                if w0 + bias0 >= 0 and w1 + bias1 >= 0 and w2 + bias2 >= 0:
+                    # TODO: perspective-correct interpolation
+                    a, b, g = w0 / area, w1 / area, w2 / area
+                    #unicornhathd.set_pixel(x, y, 255 * a, 255 * b, 255 * g)
 
 
 def fragment_shader():
@@ -116,23 +127,19 @@ def fragment_shader():
 def clip_to_screen_space(vertex):
     ndc = vertex.xyz / vertex.w
     screen = glm.vec3(0)
-    screen.x = ((viewport["w"] * 0.5) * ndc.x) + ((viewport["w"] * 0.5) + viewport["x"])
-    screen.y = ((viewport["h"] * 0.5) * ndc.y) + ((viewport["h"] * 0.5) + viewport["y"])
-    screen.z = (((viewport["f"] - viewport["n"]) * 0.5) * ndc.z) + ((viewport["f"] + viewport["n"]) * 0.5)
+    screen.x = ((view.w * 0.5) * ndc.x) + ((view.w * 0.5) + view.x)
+    screen.y = ((view.h * 0.5) * ndc.y) + ((view.h * 0.5) + view.y)
+    screen.z = (((view.f - view.n) * 0.5) * ndc.z) + ((view.f + view.n) * 0.5)
     return screen
 
 
-def barycentric(v1, v2, v3, p):
-    A_t = glm.length(glm.cross((v2 - v1), (v3 - v1))) * 0.5
-    A_1 = glm.length(glm.cross((v2 - p), (v3 - p))) * 0.5
-    A_2 = glm.length(glm.cross((p - v1), (v3 - v1))) * 0.5
-    A_3 = glm.length(glm.cross((v2 - v1), (p - v1))) * 0.5
+def edge_function(a, b, p):
+    return (b.x - a.x) * (p.y - a.y) - (b.y - a.y) * (p.x - a.x);
 
-    a = A_1 / A_t
-    b = A_2 / A_t
-    g = A_3 / A_t
 
-    return a, b, g
+def top_left_test(p, q):
+    e = q - p
+    return True if e.x < 0 and e.y == 0 or e.y < 0 else False
 
 
 if __name__ == "__main__":
